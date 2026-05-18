31856f525699420626cefda9862583e80f55344b
Luto228 <megapupsik909@gmail.com>
2026-05-18 21:41:42 +0400

    feat: delete removed the extra check, moving it to main.py

---
+++ functions/add.py
@@ -0,0 +1,30 @@
+import sys
+import shutil
+from pathlib import Path
+
+from db_services.db_text import create, save_info
+
+def handle_add(file_name: str, custom_path = None):
+    home_dir = Path.home()
+    if custom_path is None:
+        custom_path = "Desktop"
+        file_custom_path = home_dir / custom_path
+    else:
+        file_custom_path = Path(custom_path)
+    currently_file_name = file_name
+    file_path = file_custom_path / currently_file_name
+    if not file_path.is_file():
+        print(f"⛔ERROR⛔ \n i cannt find {currently_file_name} file in {file_custom_path}")
+        print(f"Please write correct name")
+        return
+    print(f"✅i found {currently_file_name} file in {file_custom_path}✅")
+    DocuMind_docs_path = Path("./docs")
+    DocuMind_docs_path.mkdir(exist_ok = True)
+    final_file_path = DocuMind_docs_path / currently_file_name
+    shutil.copy(file_path, final_file_path)
+    text_inside = final_file_path.read_text(encoding="utf-8")
+    try:
+        save_info(currently_file_name, text_inside)
+        print(f"✅file {currently_file_name} file has been created in docs✅")
+    except Exception as e:
+        print(f"Sorry! Something went wrong! \n errors name: {e}")
