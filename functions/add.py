e60801bee20a7ee598570e0f7b054277c0659cbf
Luto228 <megapupsik909@gmail.com>
2026-05-23 19:51:28 +0400

    style: The code has become more readable. Emojis have been added and spaces have been removed.

--- functions/add.py
+++ functions/add.py
@@ -16,17 +16,20 @@ def handle_add(file_name: str, custom_path = None):
     currently_file_name = file_name
     file_path = file_custom_path / currently_file_name
     if not file_path.is_file():
-        print(f"⛔ERROR⛔ \n i cannt find {currently_file_name} file in {file_custom_path}")
-        print(f"Please write correct name")
+        print(f"⛔ERROR⛔ I couldn't find {currently_file_name} in {file_custom_path}")
+        print("Please write the correct file name.")
         return
-    print(f"✅i found {currently_file_name} file in {file_custom_path}✅")
+
+    print(f"✅ I found {currently_file_name} in {file_custom_path} ✅")
+
     DocuMind_docs_path = PROJECT_ROOT / "docs"
     DocuMind_docs_path.mkdir(parents=True, exist_ok=True)
     final_file_path = DocuMind_docs_path / currently_file_name
     shutil.copy(file_path, final_file_path)
     text_inside = final_file_path.read_text(encoding="utf-8")
+
     try:
         save_info(currently_file_name, text_inside)
-        print(f"✅file {currently_file_name} file has been created in docs✅")
+        print(f"✅ File {currently_file_name} has been added to docs ✅")
     except Exception as e:
-        print(f"Sorry! Something went wrong! \n errors name: {e}")
+        print(f"⛔ERROR⛔ Something went wrong!\nError name: {e}")
